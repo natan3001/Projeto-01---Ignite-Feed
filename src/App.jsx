97088d33c8629a844713641ef4935c0ec8fce0a7
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-10 18:22:27 -0300

    Entendendo o CSS Modules, e criando o componente do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./styles.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Natan Martins"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum velit maxime alias, perferendis amet illo reprehenderit sint saepe quaerat eveniet."
