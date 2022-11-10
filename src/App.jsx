0af854bae0d97217fb800c9bc5b9fd8b0adc2948
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-10 19:32:02 -0300

    Criado o componente de sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Natan Martins"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum velit maxime alias, perferendis amet illo reprehenderit sint saepe quaerat eveniet."
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Natan Martins"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum velit maxime alias, perferendis amet illo reprehenderit sint saepe quaerat eveniet."
+          />
+        </main>
+      </div>
     </div>
   );
 }
