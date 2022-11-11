f35923afd1452ec760206ffde7e068e6ff499b46
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-11 17:26:36 -0300

    Criado o componente de avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -12,7 +13,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/natan3001.png" alt="" />
+        <Avatar src="https://github.com/natan3001.png" />
         <strong>Natan Martins</strong>
         <span>Web Developer</span>
       </div>
