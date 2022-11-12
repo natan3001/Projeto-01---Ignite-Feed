24d04b5885d49f005fd99bdce28abeda94ee9ff3
Natan Martins dos Santos <natantcr@gmail.com>
2022-11-12 17:44:21 -0300

    Adicionado a propriedade key na iterações que estavam sendo realizadas

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,7 +7,7 @@ import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
 
-export function Post({ author, publishedAt, content, key }) {
+export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
 
   const [newCommentText, setNewCommentText] = useState("");
@@ -48,10 +48,10 @@ export function Post({ author, publishedAt, content, key }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -76,7 +76,7 @@ export function Post({ author, publishedAt, content, key }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => (
-          <Comment content={comment} />
+          <Comment key={comment} content={comment} />
         ))}
       </div>
     </article>
